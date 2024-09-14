42956e4f13cf8dabe076e04eff9bd0edc05b3338
Anıl Başaran <nlbsrn@gmail.com>
2024-09-14 03:48:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import messages from "./messages";
+
+
+const App = () => {
+
+  const messi = messages.map((obj,indx)=>
+
+    <div key={indx} className=" flex p-2 m-2 rounded-lg border-2 border-slate-400">
+
+      <div className="font-bold mr-2">{obj.user}:</div>
+      <div>{obj.text}</div>
+    </div>
+    
+  );
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="outerContainer flex justify-center m-4">
+      <div className="max-w-xl w-full" >
+      {messi}
+      </div>
+      
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/messages.jsx
@@ -0,0 +1,40 @@
+const  messages =[
+    {
+      user: "max",
+      text: "Hey programmers!",
+    },
+    {
+      user: "payton",
+      text: "sup max",
+    },
+    {
+      user: "john",
+      text: "does anyone want to grab coffee on wednesday?",
+    },
+    {
+      user: "jayne",
+      text: "Im down for it! How about 1pm?",
+    },
+    {
+      user: "max",
+      text: "works for me too.",
+    },
+    {
+      user: "tai food",
+      text: "sounds good, see yall then",
+    },
+    {
+      user: "mercedes",
+      text: "I love me some gud coffee, I'll be there.",
+    },
+    {
+      user: "benz",
+      text: "I'll come a lil later with bercedes",
+    },
+    {
+      user: "taylor",
+      text: "word.",
+    },
+  ];
+  
+  export default messages;
